a16abdfd71f6c6013c1d761e6281ac1bb8141e95
Jafet Dixon Solano <jafet.dixon@gmail.com>
2020-12-17 08:31:22 -0600

    Prueba #5

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ echo "2/3 Starting the build of the react applic"
 npm run build
 
 echo "3/3 Packing the application into a docker image"
-docker build . -t star-wars-server
+docker build . -t star-wars-server:latest
 
 echo "3/3 steps completedd. GoodBye"
